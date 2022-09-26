51a99e6f1a004266d5e43d7637fe1d6bf36fdf75
ckit <a2036913@hotmail.com>
2022-09-26 19:14:04 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import TopBar from "./components/topbar/TopBar";
-import "./app.css";
 import Home from "./pages/home/Home";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
